02389d3c4d0be4c099435f87f831ca2e7112ccc2
dulithaprasanna <dulithaprasanna@users.noreply.github.com>
2026-02-04 22:39:52 +0000

    deploy: ad43a84a9d707782d49ecff0e676c99416b57e7d

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.post1.dev1+gb9570a1cf',
+    VERSION: '0.0.post1.dev1+gad43a84a9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
